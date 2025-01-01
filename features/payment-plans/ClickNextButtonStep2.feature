0a9b637f4cf8c2257d1f8116d7734b812ccc358b
MehmetArkun <mehmetarkungb@gmail.com>
2025-01-01 14:35:56 +0000

    sep16

--- features/payment-plans/ClickNextButtonStep2.feature
+++ features/payment-plans/ClickNextButtonStep2.feature
@@ -12,12 +12,40 @@ Feature: Click on the next button on payment plans page   #! Test Only
     #* AC7: By default, the pay button should be displayed.
 
 
-    #Background:
-    #    Given user is on the enrollment page
-    #    And user has completed step one with valid information
-    #    And user is on step two of the enrollment process
+    Background:
+    Given user is on the enrollment page
+    And user has completed step one with valid information
+    And user is on step two of the enrollment process
+    Then User clicks on upfront payment plan
 
 
-    #TODO: Create scenarios that cover all the acceptance criteria
+    Scenario: Verify that the next button is active after clicking on any plan
+    And The next button is clickable
+
+    Scenario: Verify that the user is redirected to the next step when clicking on the next button
+    And The user clicks on the next button
+    Then The product price is shown in middle of the page
+
+    Scenario: Verify that step 1 and step 2 are green and step 3 is blue in step 3
+    And The user clicks on the next button
+    Then The step 1 circle is green
+    And The step 2 circle is green
+    And The step 3 circle is blue
+
+    Scenario: Verify that the payment component is displayed in step 3
+    And The user clicks on the next button
+    Then The payment component is displayed
+
+    Scenario: Verify that a price summary is displayed in step 3
+    And The user clicks on the next button
+    Then The price summary is displayed
+
+    Scenario: Verify that the back button is displayed in step 3
+    And The user clicks on the next button
+    Then The back button is displayed
+
+    Scenario: Verify that the pay button is displayed in step 3
+    And The user clicks on the next button
+    Then The pay button is displayed
 
    
--- steps/payment-plans/clickNextButtonStep2.js
+++ steps/payment-plans/clickNextButtonStep2.js
@@ -1,4 +1,71 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { paymentPlanPage, page } from "../../globalPagesSetup.js";
+import { expect } from "@playwright/test";
+import {
+  paymentPlanPage,
+  page,
+  startApplicationPage,
+  reviewPaymentPage,
+} from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
+
+Given("user has completed step one with valid information", async function () {
+  await startApplicationPage.firstNameInputBox.fill("Mehmet");
+  await startApplicationPage.lastNameInputBox.fill("Arkun");
+  await startApplicationPage.emailInputBox.fill("mehmetarkungb@gmail.com");
+  await startApplicationPage.phoneNumberInputBox.fill("555-555-5555");
+  await startApplicationPage.nextButton.click();
+});
+
+Given("user is on step two of the enrollment process", async function () {
+  await expect(paymentPlanPage.chooseAPaymentPlanText).toBeVisible();
+});
+
+Then("User clicks on upfront payment plan", async function () {
+  await paymentPlanPage.upfrontPaymentOption.click();
+});
+
+Then("The next button is clickable", async function () {
+  await expect(paymentPlanPage.activeNextButton).toBeVisible();
+});
+
+Then("The user clicks on the next button", async function () {
+  await paymentPlanPage.activeNextButton.click();
+});
+
+Then("The product price is shown in middle of the page", async function () {
+  await expect(reviewPaymentPage.productPriceText).toBeVisible();
+});
+
+Then("The step 1 circle is green", async function () {
+    await expect(paymentPlanPage.step1Circle).toContainText("done");
+});
+
+Then("The step 2 circle is green", async function () {
+        await expect(paymentPlanPage.step2Circle).toContainText("done");
+
+});
+
+Then("The step 3 circle is blue", async function () {
+        await expect(paymentPlanPage.step3Circle).toContainText("edit")
+});
+
+Then("The payment component is displayed", async function () {
+  await expect(
+    reviewPaymentPage.paymentFrame.locator("//input[@id='Field-numberInput']")
+  ).toBeVisible();
+});
+
+Then("The price summary is displayed", async function () {
+    await expect(reviewPaymentPage.productPriceAmount).toBeVisible();
+    await expect(reviewPaymentPage.processingFeeAmount).toBeVisible();
+    await expect(reviewPaymentPage.totalAmount).toBeVisible();
+    await expect(reviewPaymentPage.subtotalAmount).toBeVisible();
+});
+
+Then("The back button is displayed", async function () {
+    await expect(reviewPaymentPage.backButton).toBeVisible();
+});
+
+Then("The pay button is displayed", async function () {
+    await expect(reviewPaymentPage.payButton).toBeVisible();
+});
